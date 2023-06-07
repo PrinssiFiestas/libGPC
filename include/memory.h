898277c6740704fe535bd6b8e0e2d75230c28cbb
Lauri Fiestas <89028886+PrinssiFiestas@users.noreply.github.com>
2023-06-07 21:49:38 +0300

    added memory api

--- include/memory.h
+++ include/memory.h
@@ -4,17 +4,42 @@
  * https://github.com/PrinssiFiestas/libGPC/blob/main/LICENSE.md
  */
 
-#ifndef GPC_ASSERT_H
-#define GPC_ASSERT_H
+#ifndef GPC_MEMORY_H
+#define GPC_MEMORY_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include <stddef.h>
+#include "overload.h"
 
+#define begin		{ DynamicObjOwner thisScope;
+#define ret(obj)	moveOwnership(obj, callingScope); freeAll(thisScope); return obj;
+#define end			automem_end; }
+
+typedef struct DynamicObjOwner
+{
+	void* firstObject;
+	void* lastObject;
+};
+
+// PRIVATE
+struct DynamicObjectList
+{
+	struct DynamicObjectList* previous;
+	struct DynamicObjectList* next;
+}
+
+// malloc and assign ownership
+void* automalloc(size_t, DynamicObjOwner*);
+// Prevent freeing by end, automem_end or freeAll
+void moveOwnership(void* object, DynamicObjOwner* newOwner);
+// Frees every object owned by owner
+void freeAll(DynamicObjOwner* owner);
 
 #ifdef __cplusplus
 } // extern "C"
 #endif
 
-#endif // GPC_ASSERT_H
+#endif // GPC_MEMORY_H
