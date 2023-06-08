74b837c939863c0611f73d2b408c1ba679ab1631
Lauri Fiestas <89028886+PrinssiFiestas@users.noreply.github.com>
2023-06-08 21:20:38 +0300

    better fake heap

--- tests/test_memory.c
+++ tests/test_memory.c
@@ -4,6 +4,10 @@
  * https://github.com/PrinssiFiestas/libGPC/blob/main/LICENSE.md
  */
 
+// Do not test single byte allocations! 0x00 is reserved for NULL, 0x01 for 
+// RESERVED, and 0xFF for FREED. This means that testing for single bytes will
+// fail at least 0x100/3 times randomly! Always allocate at least 4 bytes!
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -17,11 +21,11 @@
 
 // fake heap
 static uint8_t* g_testHeap;
-static const size_t TEST_HEAP_SIZE = 0xFE;
+static const size_t TEST_HEAP_SIZE = 0x100;
 
 #define EMPTY_HEAP -1
-#define FREED 0x00
-#define RESERVED 0xFF
+#define FREED 0xFF
+#define RESERVED 0x01
 
 // Returns the memory location index of first object and -1 if heap is empty
 long testHeapFirstObject()
@@ -34,34 +38,34 @@ long testHeapFirstObject()
 
 size_t objSize(void* p)
 {
+	uint64_t* ptr = (uint64_t*)p;
 	size_t size = 0;
-	uint8_t* ptr = (uint8_t*)p;
-	printf("OBJSIZE %p\n", p);
-	while (ptr[size] != FREED)
-	{
-		printf("%2X %s", ptr[size], (size+1)%8 ? "" : "\n");
+	while ((void*)ptr[size] != NULL)
 		size++;
-	}
-	puts("\n");
 	return size;
 }
+
+void printHeap(void);
 
 int main()
 {
 	// setup
-	g_testHeap = calloc(TEST_HEAP_SIZE, sizeof(void*));
+	g_testHeap = malloc(TEST_HEAP_SIZE * sizeof(void*));
+	for (size_t i = 0; i < TEST_HEAP_SIZE; i++)
+		g_testHeap[i] = FREED;
 	// pointless calloc() test because paranoia
 	ASSERT(testHeapFirstObject() EQ EMPTY_HEAP, "calloc() failed!");
 	
 	TEST(allocations)
 	{
 		begin
-			void* obj1 = scopedAlloc(3);
-			void* obj2 = scopedAlloc(5);
-			void* obj3 = scopedAlloc(4);
-			ASSERT(objSize(obj1) EQ 3);
-			ASSERT(objSize(obj2) EQ 5);
-			ASSERT(objSize(obj3) EQ 4);
+			void* obj1 = scopedAlloc(3 * sizeof(void*));
+			void* obj2 = scopedAlloc(5 * sizeof(void*));
+			void* obj3 = scopedAlloc(4 * sizeof(void*));
+			printHeap();
+			ASSERT(objSize(obj1) EQ 3 * sizeof(void*));
+			ASSERT(objSize(obj2) EQ 5 * sizeof(void*));
+			ASSERT(objSize(obj3) EQ 4 * sizeof(void*));
 		end
 		ASSERT(testHeapFirstObject() EQ EMPTY_HEAP, "Heap not empty after scope!");
 	}
@@ -79,7 +83,7 @@ void* test_malloc(size_t size)
 	// initialize freeSpace
 	static uint8_t* freeSpace = NULL;
 	if (freeSpace == NULL)
-		freeSpace = g_testHeap + 8;
+		freeSpace = g_testHeap;
 	
 	printf("MALLOC %p\n", freeSpace);
 	for (size_t i = 0; i < size; i++)
@@ -88,8 +92,8 @@ void* test_malloc(size_t size)
 		printf("%2X %s", freeSpace[i], (i+1)%8 ? "" : "\n" );
 	}
 	printf("%2X\n\n", freeSpace[size]);
-	freeSpace += 32;
-	return freeSpace - 32;
+	freeSpace += 64;
+	return freeSpace - 64;
 }
 
 void test_free(void* p)
@@ -112,4 +116,12 @@ void* test_realloc(void* p, size_t size)
 		destination[i] = ptr[i];
 	test_free(p);
 	return NULL;
+}
+
+void printHeap()
+{
+	puts("FULL HEAP");
+	for (size_t i = 0; i < TEST_HEAP_SIZE; i++)
+		printf("%2X %s", g_testHeap[i], (i+1)%8 ? "" : "\n");
+	puts("\n");
 }
