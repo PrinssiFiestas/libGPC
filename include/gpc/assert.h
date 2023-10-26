4a1ddf07dd0b9b2f76699a85db27a53db624ebe0
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2023-10-26 14:17:54 +0300

    started with assert header with docs

--- include/gpc/assert.h
+++ include/gpc/assert.h
@@ -5,4 +5,24 @@
 #ifndef GPC_ASSERT_INCLUDED
 #define GPC_ASSERT_INCLUDED 1
 
+// ----------------------------------------------------------------------------
+//
+//          CORE API
+//
+// ----------------------------------------------------------------------------
+
+// Starts test. Subsequent calls starts a new test ending the last one. If name
+// is NULL last test will be ended without starting a new test.
+void gpc_test(const char* name);
+
+// Starts suite. Subsequent calls starts a new suite ending the last one. If
+// name is NULL last suite will be ended without starting a new suite.
+void gpc_suite(const char* name);
+
+// ----------------------------------------------------------------------------
+//
+//          END OF CORE API
+//
+// ----------------------------------------------------------------------------
+
 #endif // GPC_ASSERT_INCLUDED
