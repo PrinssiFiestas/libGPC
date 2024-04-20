1ac9d18114872df4ff986a67cbccb7184efc4f28
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-04-20 17:07:11 +0300

    Removed unused files

--- include/gpc/error.h
+++
@@ -1,8 +0,0 @@
-// MIT License
-// Copyright (c) 2023 Lauri Lorenzo Fiestas
-// https://github.com/PrinssiFiestas/libGPC/blob/main/LICENSE.md
-
-#ifndef GPC_ERROR_H
-#define GPC_ERROR_H
-
-#endif // GPC_ERROR_H
--- src/error.c
+++
@@ -1,3 +0,0 @@
-// MIT License
-// Copyright (c) 2023 Lauri Lorenzo Fiestas
-// https://github.com/PrinssiFiestas/libGPC/blob/main/LICENSE.md
