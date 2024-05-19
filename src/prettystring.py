180273621688c288d786f3fe743a6ae9de98fdea
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-05-19 21:52:05 +0300

    Added GDB pretty printer for GPString

---
+++ src/prettystring.py
@@ -0,0 +1,23 @@
+import gdb
+import gdb.printing
+
+class StringPrinter():
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        T_str_data      = gdb.lookup_type("GPStringHeader")
+        val_as_data_ptr = self.val.cast(T_str_data.pointer())
+        str_data        = (val_as_data_ptr - 1).dereference()
+
+        T_cstr       = gdb.lookup_type("char").pointer()
+        val_contents = self.val.cast(T_cstr).string("utf-8", length = str_data["length"])
+        val_contents = str(val_contents).encode("unicode_escape").decode()
+        return "\"" + val_contents + "\", " + str(str_data)
+
+def str_lookup_function(val):
+    if str(val.type) == "GPString":
+        return StringPrinter(val)
+    return None
+
+gdb.pretty_printers.append(str_lookup_function)
