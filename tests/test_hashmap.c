5166234e5200a7911eae755adf67c88c9e7d6b99
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-06-09 13:33:45 +0300

    Added tests to hash map

--- tests/test_hashmap.c
+++ tests/test_hashmap.c
@@ -5,26 +5,144 @@
 #include "../src/hashmap.c"
 #include <gpc/assert.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct my_type
+{
+    char* data;
+    size_t length;
+    bool is_destroyed;
+} MyType;
+
+MyType create(const char* init)
+{
+    return (MyType){ strcpy(malloc(strlen(init) + 1), init), strlen(init) };
+}
+
+// Really takes MyType* as argument but void* avoids awkward func pointer cast
+void destroy(void*_obj)
+{
+    MyType* obj = _obj;
+    free(obj->data);
+    obj->is_destroyed = true;
+}
 
 int main(void)
 {
+    GPArena _arena = gp_arena_new(0);
+    GPAllocator* arena = (GPAllocator*)&_arena;
+
     gp_suite("Hash map");
     {
-        GPHashMap* map = gp_hash_map_new(
-            gp_heap, &(GPMapInitializer){ sizeof(int) });
-
         const char* key = "This is my key!";
-        gp_expect(gp_hash_map_get(map, key, strlen(key)) == NULL,
-            "No elements yet");
+        gp_test("Pointer elements");
+        {
+            // Maps store pointers by default.
+            GPHashMap* cstr_map = gp_hash_map_new(arena, NULL);
 
-        gp_hash_map_put(map, key, strlen(key), &(int){3});
-        gp_expect(*(int*)gp_hash_map_get(map, key, strlen(key)) == 3);
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == NULL,
+                "No elements yet");
 
-        gp_hash_map_remove(map, key, strlen(key));
-        gp_expect(gp_hash_map_get(map, key, strlen(key)) == NULL,
-            "Element removed!");
+            const char* cstr = "I am the element!";
+            gp_hash_map_put(cstr_map, key, strlen(key), cstr);
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == cstr);
 
-        gp_hash_map_delete(map);
+            gp_hash_map_remove(cstr_map, key, strlen(key));
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == NULL,
+                "Element removed.");
+        }
+
+        gp_test("Integers as pointer elements");
+        {
+            GPHashMap* int_map = gp_hash_map_new(arena, NULL);
+
+            gp_expect(gp_hash_map_get(int_map, key, strlen(key)) == NULL,
+                "No elements yet");
+
+            // Putting and getting integers as pointers by using casts can save
+            // memory.
+            gp_hash_map_put(int_map, key, strlen(key), (void*)7);
+            gp_expect((intptr_t)gp_hash_map_get(int_map, key, strlen(key)) == 7);
+
+            gp_hash_map_remove(int_map, key, strlen(key));
+
+            // However, now the return value of get() can not be used to check
+            // if element is removed. The element could just be set to 0.
+            gp_expect(gp_hash_map_get(int_map, key, strlen(key)) == NULL,
+                "Element removed??");
+        }
+
+        gp_test("Destructor");
+        {
+            // Since elements are pointers by default, free() is a valid
+            // destructor.
+            GPHashMap* cstr_map = gp_hash_map_new(
+                arena, &(GPMapInitializer){.destructor = free });
+
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == NULL,
+                "No elements yet");
+
+            const char cstr_init[] = "C-string on heap!";
+            char* cstr = strcpy(malloc(sizeof cstr_init), cstr_init);
+            gp_hash_map_put(cstr_map, key, strlen(key), cstr);
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == cstr);
+
+            gp_hash_map_remove(cstr_map, key, strlen(key));
+
+            gp_expect(gp_hash_map_get(cstr_map, key, strlen(key)) == NULL,
+                "Element removed.");
+
+            // The hash maps in the tests above omitted cleanup since they were
+            // allocated in the arena. Here, the objects themselves are pointers
+            // to objects in heap so cleanup is essential to make sure that
+            // free() is being called for all elements even when the map itself
+            // is in arena.
+            gp_hash_map_delete(cstr_map);
+        }
+
+        gp_test("Sized elements");
+        {
+            // Providing a size for elements tells the map to store the actual
+            // elements in the map, not pointers to them. This is especially
+            // useful with types that don't fit to sizeof(void*) so they do not
+            // need a separate allocation.
+            GPHashMap* map = gp_hash_map_new(
+                arena, &(GPMapInitializer){
+                    .element_size = sizeof(MyType), .destructor = destroy });
+
+            const char* key1 = "Key to first object";
+            const char* key2 = "Key to second object";
+            // The scoping operator {} is used to emphasize that init_values are
+            // copied to appropriate slot in map and are not being used after.
+            {
+                MyType init_values = create("Some stuff");
+                gp_hash_map_put(map, key1, strlen(key1), &init_values);
+                init_values = create("Some other stuff");
+                gp_hash_map_put(map, key2, strlen(key2), &init_values);
+            }
+            MyType* obj1 = gp_hash_map_get(map, key1, strlen(key1));
+            gp_expect(memcmp(obj1->data, "Some stuff", obj1->length) == 0);
+            gp_expect( ! obj1->is_destroyed);
+            MyType* obj2 = gp_hash_map_get(map, key2, strlen(key2));
+            gp_expect(memcmp(obj2->data, "Some other stuff", obj2->length) == 0);
+            gp_expect( ! obj2->is_destroyed);
+
+            gp_hash_map_remove(map, key1, strlen(key1));
+            gp_expect(gp_hash_map_get(map, key1, strlen(key1)) == NULL,
+                "Element removed.");
+
+            // Again, size given in map initializer means that the objects are
+            // stored in the map so there is no use-after-free here. However,
+            // the destructor has been called and any store using key1 will
+            // of course overwrite whatever obj1 is pointing to.
+            gp_expect(obj1->is_destroyed);
+
+            gp_hash_map_delete(map);
+
+            // No use after free here since the map is stored in the arena.
+            // However, destructor for all objects have been run.
+            gp_expect(obj2->is_destroyed);
+        }
     }
 
     gp_suite("Non-hashed map");
@@ -65,4 +183,6 @@ int main(void)
         }
     }
     #endif
+
+    gp_arena_delete(&_arena);
 }
