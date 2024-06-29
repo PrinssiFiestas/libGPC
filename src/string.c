dac7e4e114c8b5f9d379e4a1bf3f1aac413791d6
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-06-29 14:29:08 +0300

    equal_case() now uses Unicode simple folding

--- src/string.c
+++ src/string.c
@@ -21,6 +21,7 @@
 #include <sys/stat.h>
 #include "thread.h"
 
+#if 0 // unused for now, maybe later
 #if _WIN32
 typedef _locale_t GPLocale;
 #define towlower_l(...) _towlower_l(__VA_ARGS__)
@@ -37,7 +38,6 @@ static GPLocale gp_locale_new(int category, const char*const locale)
     return newlocale(category, locale, (GPLocale)0);
     #endif
 }
-#if 0 // unused for now, maybe later
 static void gp_locale_delete(GPLocale locale)
 {
     #if _WIN32
@@ -46,17 +46,17 @@ static void gp_locale_delete(GPLocale locale)
     freelocale(locale);
     #endif
 }
-#endif
 static GPLocale gp_default_locale;
 static GPThreadOnce gp_locale_once = GP_THREAD_ONCE_INIT;
-
-extern inline void gp_str_delete(GPString);
-extern inline void gp_str_ptr_delete(GPString*);
 
 static void gp_init_default_locale(void)
 {
     gp_default_locale = gp_locale_new(LC_COLLATE, "C.UTF-8");
 }
+#endif
+
+extern inline void gp_str_delete(GPString);
+extern inline void gp_str_ptr_delete(GPString*);
 
 GPString gp_str_new(
     const GPAllocator*const allocator,
@@ -146,54 +146,48 @@ bool gp_str_equal(
         return memcmp(s1, s2, s2_size) == 0;
 }
 
+size_t gp_utf8_encode(uint32_t* encoding, const void*const u8, const size_t i);
+static uint32_t gp_u32_simple_fold(uint32_t r);
+
 bool gp_str_equal_case(
     GPString    s1,
     const void* s2,
     size_t      s2_size)
 {
-    gp_thread_once(&gp_locale_once, gp_init_default_locale);
-
-    size_t s1_length = gp_bytes_codepoint_count(s1, gp_str_length(s1));
-    size_t s2_length = gp_bytes_codepoint_count(s2, s2_size);
+    const size_t s1_length = gp_bytes_codepoint_count(s1, gp_str_length(s1));
+    const size_t s2_length = gp_bytes_codepoint_count(s2, s2_size);
     if (s1_length != s2_length)
         return false;
 
-    mbstate_t state1 = {0};
-    mbstate_t state2 = {0};
-    wchar_t wc1;
-    wchar_t wc2;
     for (size_t i = 0; i < s1_length; i++)
     {
-        size_t wc1_length = mbrtowc(&wc1, (char*)s1, sizeof(wchar_t), &state1);
-        size_t wc2_length = mbrtowc(&wc2, (char*)s2, sizeof(wchar_t), &state2);
-        if (sizeof(wchar_t) < sizeof(uint32_t)/* Windows probably */&&
-            (wc1_length == (size_t)-2) != (wc2_length == (size_t)-2))
-        { // one fits to wchar_t and other doesn't so most likely different
+        uint32_t codepoint1;
+        uint32_t codepoint2;
+        const size_t s1_codepoint_size = gp_utf8_encode(&codepoint1, s1, 0);
+        const size_t s2_codepoint_size = gp_utf8_encode(&codepoint2, s2, 0);
+        s1 += s1_codepoint_size;
+        s2 = (uint8_t*)s2 + s2_codepoint_size;
+
+        if (codepoint1 == codepoint2)
+            continue;
+        if (codepoint2 < codepoint1) { // simplify the following
+            uint32_t swap = codepoint1;
+            codepoint1 = codepoint2;
+            codepoint2 = swap;
+        }
+        if (codepoint2 < 0x80) {
+            if ('A' <= codepoint1 && codepoint1 <= 'Z' &&
+                codepoint2 == codepoint1 + 'a' - 'A')
+                continue;
             return false;
         }
-        else if (sizeof(wchar_t) < sizeof(uint32_t) &&
-                 wc1_length == (size_t)-2) // char wider than sizeof(wchar_t)
-        {                                  // so just compare raw bytes
-            size_t s1_codepoint_size = gp_str_codepoint_length(s1, 0);
-            size_t s2_codepoint_size = gp_str_codepoint_length((GPString)s2, 0);
-            if (s1_codepoint_size != s2_codepoint_size ||
-                memcmp(s1, s2, s1_codepoint_size) != 0)
-            {
-                return false;
-            }
-            s1 += s1_codepoint_size;
-            s2 = (uint8_t*)s2 + s2_codepoint_size;
-        }
-        else
-        {
-            wc1 = towlower_l(wc1, gp_default_locale);
-            wc2 = towlower_l(wc2, gp_default_locale);
-            if (wc1 != wc2)
-                return false;
+        uint32_t cp = gp_u32_simple_fold(codepoint1);
+        while (cp != codepoint1 && cp < codepoint2)
+            cp = gp_u32_simple_fold(cp);
+        if (cp == codepoint2)
+            continue;
 
-            s1 += wc1_length;
-            s2 = (uint8_t*)s2 + wc2_length;
-        }
+        return false;
     }
     return true;
 }
@@ -614,24 +608,32 @@ void gp_str_trim(
     gp_str_header(*str)->length = length;
 }
 
+size_t gp_utf8_encode(uint32_t* encoding, const void*const _u8, const size_t i)
+{
+    const GPString u8 = (GPString)_u8;
+    *encoding = 0;
+    const size_t codepoint_length = gp_str_codepoint_length(u8, i);
+    for (size_t j = 0; j < codepoint_length; j++)
+        *encoding = (*encoding << 8) | u8[i + j].c;
+
+    if (*encoding > 0x7F)
+    {
+        uint32_t mask = (*encoding <= 0x00EFBFBF) ? 0x000F0000 : 0x003F0000 ;
+        *encoding = ((*encoding & 0x07000000) >> 6) |
+            ((*encoding & mask )             >> 4) |
+            ((*encoding & 0x00003F00)        >> 2) |
+            (*encoding & 0x0000003F);
+    }
+    return codepoint_length;
+}
+
 GPArray(uint32_t) gp_utf8_to_utf32(const GPAllocator* allocator, const GPString u8)
 {
     GPArray(uint32_t) u32 = gp_arr_new(allocator, sizeof u32[0], gp_str_length(u8));
     for (size_t i = 0, codepoint_length; i < gp_str_length(u8); i += codepoint_length)
     {
-        uint32_t encoding = 0;
-        codepoint_length = gp_str_codepoint_length(u8, i);
-        for (size_t j = 0; j < codepoint_length; j++)
-            encoding = (encoding << 8) | u8[i + j].c;
-
-        if (encoding > 0x7F)
-        {
-            uint32_t mask = (encoding <= 0x00EFBFBF) ? 0x000F0000 : 0x003F0000 ;
-            encoding = ((encoding & 0x07000000) >> 6) |
-                ((encoding & mask )             >> 4) |
-                ((encoding & 0x00003F00)        >> 2) |
-                (encoding & 0x0000003F);
-        }
+        uint32_t encoding;
+        codepoint_length = gp_utf8_encode(&encoding, u8, i);
         u32[((GPArrayHeader*)u32 - 1)->length++] = encoding;
     }
     return u32;
@@ -813,7 +815,9 @@ int gp_str_case_compare( // TODO figure the API out and make this public
     mbsrtowcs(buf1, &(const char*){s1}, buf1_cap, &(mbstate_t){0});
     mbsrtowcs(buf2, &(const char*){s2}, buf2_cap, &(mbstate_t){0});
 
-    #if _WIN32
+    #if 1
+    int result = wcscoll(buf1, buf2);
+    #elif _WIN32
     int result = _wcsicoll_l(buf1, buf2, gp_default_locale);
     #else
     int result = wcscasecmp_l(buf1, buf2, gp_default_locale);
@@ -881,7 +885,7 @@ int gp_str_file(
 }
 
 // ----------------------------------------------------------------------------
-// Modified towupper() and towlower() from
+// towupper() and towlower() from
 // https://chromium.googlesource.com/native_client/nacl-newlib/+/refs/heads/main/newlib/libc/ctype
 
 /* Copyright (c) 2002 Red Hat Incorporated.
@@ -1886,4 +1890,115 @@ static uint32_t gp_u32_to_lower(uint32_t c)
 	return (c + 0x28);
     }
   return c;
+}
+
+// ----------------------------------------------------------------------------
+// Case folding from Go source code
+// Copyright (c) 2009 The Go Authors. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//    * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//    * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+static const uint16_t gp_ascii_fold[] = {
+    0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007,
+    0x0008, 0x0009, 0x000A, 0x000B, 0x000C, 0x000D, 0x000E, 0x000F,
+    0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017,
+    0x0018, 0x0019, 0x001A, 0x001B, 0x001C, 0x001D, 0x001E, 0x001F,
+    0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027,
+    0x0028, 0x0029, 0x002A, 0x002B, 0x002C, 0x002D, 0x002E, 0x002F,
+    0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037,
+    0x0038, 0x0039, 0x003A, 0x003B, 0x003C, 0x003D, 0x003E, 0x003F,
+    0x0040, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067,
+    0x0068, 0x0069, 0x006A, 0x006B, 0x006C, 0x006D, 0x006E, 0x006F,
+    0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077,
+    0x0078, 0x0079, 0x007A, 0x005B, 0x005C, 0x005D, 0x005E, 0x005F,
+    0x0060, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047,
+    0x0048, 0x0049, 0x004A, 0x212A, 0x004C, 0x004D, 0x004E, 0x004F,
+    0x0050, 0x0051, 0x0052, 0x017F, 0x0054, 0x0055, 0x0056, 0x0057,
+    0x0058, 0x0059, 0x005A, 0x007B, 0x007C, 0x007D, 0x007E, 0x007F,
+};
+
+typedef struct gp_fold_pair
+{
+    uint16_t from;
+    uint16_t to;
+} GPFoldPair;
+
+static GPFoldPair gp_case_orbit[] = {
+    {0x004B, 0x006B}, {0x0053, 0x0073}, {0x006B, 0x212A}, {0x0073, 0x017F},
+    {0x00B5, 0x039C}, {0x00C5, 0x00E5}, {0x00DF, 0x1E9E}, {0x00E5, 0x212B},
+    {0x0130, 0x0130}, {0x0131, 0x0131}, {0x017F, 0x0053}, {0x01C4, 0x01C5},
+    {0x01C5, 0x01C6}, {0x01C6, 0x01C4}, {0x01C7, 0x01C8}, {0x01C8, 0x01C9},
+    {0x01C9, 0x01C7}, {0x01CA, 0x01CB}, {0x01CB, 0x01CC}, {0x01CC, 0x01CA},
+    {0x01F1, 0x01F2}, {0x01F2, 0x01F3}, {0x01F3, 0x01F1}, {0x0345, 0x0399},
+    {0x0392, 0x03B2}, {0x0395, 0x03B5}, {0x0398, 0x03B8}, {0x0399, 0x03B9},
+    {0x039A, 0x03BA}, {0x039C, 0x03BC}, {0x03A0, 0x03C0}, {0x03A1, 0x03C1},
+    {0x03A3, 0x03C2}, {0x03A6, 0x03C6}, {0x03A9, 0x03C9}, {0x03B2, 0x03D0},
+    {0x03B5, 0x03F5}, {0x03B8, 0x03D1}, {0x03B9, 0x1FBE}, {0x03BA, 0x03F0},
+    {0x03BC, 0x00B5}, {0x03C0, 0x03D6}, {0x03C1, 0x03F1}, {0x03C2, 0x03C3},
+    {0x03C3, 0x03A3}, {0x03C6, 0x03D5}, {0x03C9, 0x2126}, {0x03D0, 0x0392},
+    {0x03D1, 0x03F4}, {0x03D5, 0x03A6}, {0x03D6, 0x03A0}, {0x03F0, 0x039A},
+    {0x03F1, 0x03A1}, {0x03F4, 0x0398}, {0x03F5, 0x0395}, {0x0412, 0x0432},
+    {0x0414, 0x0434}, {0x041E, 0x043E}, {0x0421, 0x0441}, {0x0422, 0x0442},
+    {0x042A, 0x044A}, {0x0432, 0x1C80}, {0x0434, 0x1C81}, {0x043E, 0x1C82},
+    {0x0441, 0x1C83}, {0x0442, 0x1C84}, {0x044A, 0x1C86}, {0x0462, 0x0463},
+    {0x0463, 0x1C87}, {0x1C80, 0x0412}, {0x1C81, 0x0414}, {0x1C82, 0x041E},
+    {0x1C83, 0x0421}, {0x1C84, 0x1C85}, {0x1C85, 0x0422}, {0x1C86, 0x042A},
+    {0x1C87, 0x0462}, {0x1C88, 0xA64A}, {0x1E60, 0x1E61}, {0x1E61, 0x1E9B},
+    {0x1E9B, 0x1E60}, {0x1E9E, 0x00DF}, {0x1FBE, 0x0345}, {0x2126, 0x03A9},
+    {0x212A, 0x004B}, {0x212B, 0x00C5}, {0xA64A, 0xA64B}, {0xA64B, 0x1C88},
+};
+
+static uint32_t gp_u32_simple_fold(uint32_t r)
+{
+	if (r < sizeof gp_ascii_fold / sizeof*gp_ascii_fold) {
+		return gp_ascii_fold[r];
+	}
+
+	// Consult caseOrbit table for special cases.
+	uint32_t lo = 0;
+	uint32_t hi = sizeof gp_case_orbit / sizeof*gp_case_orbit;
+	for (; lo < hi;) {
+		uint32_t m = (lo+hi) >> 1;
+		if (gp_case_orbit[m].from < r) {
+			lo = m + 1;
+		} else {
+			hi = m;
+		}
+	}
+	if (lo < sizeof gp_case_orbit / sizeof*gp_case_orbit && gp_case_orbit[lo].from == r) {
+		return gp_case_orbit[lo].to;
+	}
+
+	// No folding specified. This is a one- or two-element
+	// equivalence class containing rune and ToLower(rune)
+	// and ToUpper(rune) if they are different from rune.
+        uint32_t l = gp_u32_to_lower(r);
+	if (l != r) {
+		return l;
+	}
+	return gp_u32_to_upper(r);
 }
