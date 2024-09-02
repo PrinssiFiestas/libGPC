aa1073c09a34a6ca82040373419333db8a0237da
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-09-02 10:19:03 +0300

    Made unit tests slightly more thread safe

--- src/assert.c
+++ src/assert.c
@@ -28,7 +28,7 @@ static GP_MAYBE_ATOMIC uint32_t gp_test_count    = 0;
 static GP_MAYBE_ATOMIC uint32_t gp_suite_count   = 0;
 static GP_MAYBE_ATOMIC uint32_t gp_tests_failed  = 0;
 static GP_MAYBE_ATOMIC uint32_t gp_suites_failed = 0;
-static GP_MAYBE_ATOMIC uint32_t gp_initialized_testing = false; // TODO thread once
+static GPThreadOnce gp_tests_once  = GP_THREAD_ONCE_INIT;
 static GPThreadOnce gp_colors_once = GP_THREAD_ONCE_INIT;
 #define GP_FAILED_STR GP_RED          "[FAILED]" GP_RESET_TERMINAL
 #define GP_PASSED_STR GP_BRIGHT_GREEN "[PASSED]" GP_RESET_TERMINAL
@@ -45,27 +45,26 @@ void gp_end_testing(void)
     gp_test(NULL);
     gp_suite(NULL);
 
-    printf("Finished testing%s%s\n", *prog_name ? " in " : ".", prog_name);
-    printf("A total of %u tests ran in %u suites\n", gp_test_count, gp_suite_count);
+    pf_printf("Finished testing%s%s\n", *prog_name ? " in " : ".", prog_name);
+    pf_printf("A total of %u tests ran in %u suites\n", gp_test_count, gp_suite_count);
 
     if (gp_tests_failed || gp_suites_failed)
-        fprintf(stderr,
+        pf_fprintf(stderr,
             GP_RED "%u tests failed and %u suites failed!" GP_RESET_TERMINAL "\n",
             gp_tests_failed, gp_suites_failed);
     else
-        printf(GP_BRIGHT_GREEN "Passed all tests!" GP_RESET_TERMINAL "\n");
+        pf_printf(GP_BRIGHT_GREEN "Passed all tests!" GP_RESET_TERMINAL "\n");
 
     puts("---------------------------------------------------------------");
 
     if (gp_tests_failed || gp_suites_failed)
         exit(EXIT_FAILURE);
 
-    // Prevent redundant reporting at exit. Also user may want to restart tests.
+    // Prevent redundant reporting at exit.
     gp_test_count    = 0;
     gp_suite_count   = 0;
     gp_tests_failed  = 0;
     gp_suites_failed = 0;
-    gp_initialized_testing = false;
 }
 
 void gp_enable_terminal_colors(void)
@@ -79,41 +78,34 @@ void gp_enable_terminal_colors(void)
     #endif
 }
 
-// TODO use thread once
 static void gp_init_testing(void)
 {
-    if ( ! gp_initialized_testing)
-    {
-        #if _WIN32
-        gp_thread_once(&gp_colors_once, gp_enable_terminal_colors);
-        #endif
-        gp_initialized_testing = true;
+    gp_thread_once(&gp_colors_once, gp_enable_terminal_colors);
 
-        #if (__GNUC__ && __linux__) || BSD
-        extern const char* __progname;
-        prog_name = __progname;
-        #elif _WIN32
-        static char prog_name_buf[MAX_PATH] = "";
-        size_t length = GetModuleFileNameA(NULL, prog_name_buf, MAX_PATH);
+    #if (__GNUC__ && __linux__) || BSD
+    extern const char* __progname;
+    prog_name = __progname;
+    #elif _WIN32
+    static char prog_name_buf[MAX_PATH] = "";
+    size_t length = GetModuleFileNameA(NULL, prog_name_buf, MAX_PATH);
 
-        bool valid_ascii = 0 < length && length < MAX_PATH;
-        for (size_t i = 0; i < length && valid_ascii; i++)
-            valid_ascii = ~prog_name_buf[i] & 0x80;
-        if (valid_ascii) {
-            const char* trimmed = strrchr(prog_name_buf, '\\');
-            prog_name = trimmed ? trimmed + strlen("\\") : prog_name_buf;
-        }
-        #endif
-
-        puts("---------------------------------------------------------------");
-        printf("Starting tests%s%s\n\n", *prog_name ? " in " : "", prog_name);
-        atexit(gp_end_testing);
+    bool valid_ascii = 0 < length && length < MAX_PATH;
+    for (size_t i = 0; i < length && valid_ascii; i++)
+        valid_ascii = ~prog_name_buf[i] & 0x80;
+    if (valid_ascii) {
+        const char* trimmed = strrchr(prog_name_buf, '\\');
+        prog_name = trimmed ? trimmed + strlen("\\") : prog_name_buf;
     }
+    #endif
+
+    puts("---------------------------------------------------------------");
+    pf_printf("Starting tests%s%s\n\n", *prog_name ? " in " : "", prog_name);
+    atexit(gp_end_testing);
 }
 
 void gp_test(const char* name)
 {
-    gp_init_testing();
+    gp_thread_once(&gp_tests_once, gp_init_testing);
 
     // End current test
     if (gp_current_test != NULL)
@@ -121,21 +113,17 @@ void gp_test(const char* name)
         const char* indent = gp_current_suite == NULL ? "" : "\t";
         if (gp_test_failed) {
             gp_tests_failed++;
-            fprintf(stderr,
+            pf_fprintf(stderr,
             "%s" GP_FAILED_STR " test " GP_CYAN "%s" GP_RESET_TERMINAL "\n", indent, gp_current_test);
         } else {
-            printf(
+            pf_printf(
             "%s" GP_PASSED_STR " test " GP_CYAN "%s" GP_RESET_TERMINAL "\n", indent, gp_current_test);
         }
-
         gp_current_test = NULL;
     }
-
     // Start new test
-    if (name != NULL)
-    {
+    if (name != NULL) {
         // No starting message cluttering output
-
         gp_current_test = name;
         gp_test_failed = false;
         gp_test_count++;
@@ -144,7 +132,7 @@ void gp_test(const char* name)
 
 void gp_suite(const char* name)
 {
-    gp_init_testing();
+    gp_thread_once(&gp_tests_once, gp_init_testing);
     gp_test(NULL); // End current test
 
     // End current suite
@@ -152,9 +140,9 @@ void gp_suite(const char* name)
     {
         if (gp_suite_failed) {
             gp_suites_failed++;
-            fprintf(stderr, GP_FAILED_STR " suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n\n", gp_current_suite);
+            pf_fprintf(stderr, GP_FAILED_STR " suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n\n", gp_current_suite);
         } else {
-            printf(GP_PASSED_STR " suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n\n", gp_current_suite);
+            pf_printf(GP_PASSED_STR " suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n\n", gp_current_suite);
         }
         gp_current_suite = NULL;
     }
@@ -162,7 +150,7 @@ void gp_suite(const char* name)
     // Start new suite
     if (name != NULL)
     {
-        printf("Starting suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n", name);
+        pf_printf("Starting suite " GP_CYAN "%s" GP_RESET_TERMINAL "\n", name);
 
         gp_current_suite = name;
         gp_suite_failed = false;
@@ -205,7 +193,7 @@ void gp_fail_internal(
         (void)va_arg(args.list, uint32_t);
 
     const char* indent = gp_current_test != NULL ? "\t" : "";
-    fprintf(stderr,
+    pf_fprintf(stderr,
         "%s%s " GP_WHITE_BG GP_BLACK "line %i" GP_RESET_TERMINAL
         " in " GP_CYAN "%s" GP_RESET_TERMINAL "\n"
         "%sCondition " GP_RED "%s " GP_FAILED_STR "\n",
@@ -237,7 +225,7 @@ void gp_fail_internal(
                         asterisk_count++;
 
                     if (fmt_spec == NULL) {
-                        fprintf(stderr, "Invalid format specifier \"%s\".", fmt);
+                        pf_fprintf(stderr, "Invalid format specifier \"%s\".", fmt);
                         continue;
                     }
                 }
@@ -245,26 +233,26 @@ void gp_fail_internal(
             size_t printed = 0;
             if (fmt_spec_count == 0) // user comment
             {
-                fprintf(stderr, "%s\n", fmt);
+                pf_fprintf(stderr, "%s\n", fmt);
                 continue;
             }
             else if (fmt_spec_count == 1 && asterisk_count == 0)
             {
-                fprintf(stderr,
+                pf_fprintf(stderr,
                     GP_BRIGHT_WHITE "%s" GP_RESET_TERMINAL " = ",
                     objs[i + 1/*0 is fmt so next one*/].identifier);
 
                 // Color and opening quote if string or char
                 if (*fmt_spec == 'c') // character
-                    fprintf(stderr, GP_YELLOW);
+                    pf_fprintf(stderr, GP_YELLOW);
                 else if (*fmt_spec == 's' || *fmt_spec == 'S') // string
-                    fprintf(stderr, GP_BRIGHT_RED);
+                    pf_fprintf(stderr, GP_BRIGHT_RED);
                 else if (strchr("dibBouxX", *fmt_spec)) // integer
-                    fprintf(stderr, GP_BRIGHT_BLUE);
+                    pf_fprintf(stderr, GP_BRIGHT_BLUE);
                 else if (strchr("fFeEgG", *fmt_spec)) // floating point
-                    fprintf(stderr, GP_BRIGHT_MAGENTA);
+                    pf_fprintf(stderr, GP_BRIGHT_MAGENTA);
                 else if (*fmt_spec == 'p') // pointer
-                    fprintf(stderr, GP_BLUE);
+                    pf_fprintf(stderr, GP_BLUE);
             }
             else
             {
@@ -290,11 +278,11 @@ void gp_fail_internal(
                         j++;
                     if (j >= fmt_spec_count + asterisk_count - 1)
                         break;
-                    printed += fprintf(stderr,"%s, ",objs[i + 1 + j].identifier);
+                    printed += pf_fprintf(stderr,"%s, ",objs[i + 1 + j].identifier);
                     j++;
                     _fmt++;
                 }
-                printed += fprintf(stderr,
+                printed += pf_fprintf(stderr,
                     "%s", objs[i + fmt_spec_count + asterisk_count].identifier);
 
                 if (brace != NULL) {
@@ -306,7 +294,7 @@ void gp_fail_internal(
                     fputc(r_braces[brace_i], stderr);
                     printed++;
                 }
-                fprintf(stderr, GP_RESET_TERMINAL " = " GP_BRIGHT_CYAN);
+                pf_fprintf(stderr, GP_RESET_TERMINAL " = " GP_BRIGHT_CYAN);
                 printed += strlen(" = ");
             }
 
@@ -316,18 +304,18 @@ void gp_fail_internal(
                     buf, buf_capacity = gp_next_power_of_2(required_capacity));
             }
             if (printed + required_capacity > 120)
-                fprintf(stderr, "\n\t");
+                pf_fprintf(stderr, "\n\t");
 
             pf_vsnprintf_consuming(buf, buf_capacity, fmt, &args);
-            fprintf(stderr, "%s", buf);
+            pf_fprintf(stderr, "%s", buf);
 
-            fprintf(stderr, GP_RESET_TERMINAL "\n");
+            pf_fprintf(stderr, GP_RESET_TERMINAL "\n");
 
             i += fmt_spec_count + asterisk_count;
             continue;
         } // end if string literal
 
-        fprintf(stderr,
+        pf_fprintf(stderr,
             GP_BRIGHT_WHITE "%s" GP_RESET_TERMINAL " = ", objs[i].identifier);
 
         switch (objs[i].type)
@@ -335,76 +323,76 @@ void gp_fail_internal(
             case GP_CHAR:
             case GP_SIGNED_CHAR:
             case GP_UNSIGNED_CHAR:
-                fprintf(stderr,
+                pf_fprintf(stderr,
                     GP_YELLOW "\'%c\'", (char)va_arg(args.list, int));
                 break;
 
             case GP_UNSIGNED_SHORT:
             case GP_UNSIGNED:
-                fprintf(stderr, GP_BRIGHT_BLUE "%u", va_arg(args.list, unsigned));
+                pf_fprintf(stderr, GP_BRIGHT_BLUE "%u", va_arg(args.list, unsigned));
                 break;
 
             case GP_UNSIGNED_LONG:
-                fprintf(stderr,
+                pf_fprintf(stderr,
                     GP_BRIGHT_BLUE "%lu", va_arg(args.list, unsigned long));
                 break;
 
             case GP_UNSIGNED_LONG_LONG:
-                fprintf(stderr,
+                pf_fprintf(stderr,
                     GP_BRIGHT_BLUE "%llu", va_arg(args.list, unsigned long long));
                 break;
 
             case GP_BOOL:
-                fprintf(stderr, va_arg(args.list, int) ? "true" : "false");
+                pf_fprintf(stderr, va_arg(args.list, int) ? "true" : "false");
                 break;
 
             case GP_SHORT:
             case GP_INT:
-                fprintf(stderr, GP_BRIGHT_BLUE "%i", va_arg(args.list, int));
+                pf_fprintf(stderr, GP_BRIGHT_BLUE "%i", va_arg(args.list, int));
                 break;
 
             case GP_LONG:
-                fprintf(stderr, GP_BRIGHT_BLUE "%li", va_arg(args.list, long));
+                pf_fprintf(stderr, GP_BRIGHT_BLUE "%li", va_arg(args.list, long));
                 break;
 
             case GP_LONG_LONG:
-                fprintf(stderr, GP_BRIGHT_BLUE "%lli", va_arg(args.list, long long));
+                pf_fprintf(stderr, GP_BRIGHT_BLUE "%lli", va_arg(args.list, long long));
                 break;
 
             double f;
             case GP_FLOAT:
             case GP_DOUBLE:
                 f = va_arg(args.list, double);
-                fprintf(stderr, GP_BRIGHT_MAGENTA "%g", f);
+                pf_fprintf(stderr, GP_BRIGHT_MAGENTA "%g", f);
                 if (f - (int64_t)f == f/* whole number */&&
                     (int64_t)f < 100000) { // not printed using %e style
-                    fprintf(stderr, ".0");
+                    pf_fprintf(stderr, ".0");
                 } break;
 
             const char* char_ptr;
             case GP_CHAR_PTR:
                 char_ptr = va_arg(args.list, char*);
                 if (char_ptr != NULL)
-                    fprintf(stderr, GP_BRIGHT_RED "\"%s\"", char_ptr);
+                    pf_fprintf(stderr, GP_BRIGHT_RED "\"%s\"", char_ptr);
                 else
-                    fprintf(stderr, GP_BRIGHT_RED "(null)");
+                    pf_fprintf(stderr, GP_BRIGHT_RED "(null)");
                 break;
 
             GPString str;
             case GP_STRING:
                 str = va_arg(args.list, GPString);
                 if (str != NULL)
-                    fprintf(stderr, GP_BRIGHT_RED "\"%.*s\"",
+                    pf_fprintf(stderr, GP_BRIGHT_RED "\"%.*s\"",
                         (int)gp_str_length(str), (char*)str);
                 else
-                    fprintf(stderr, GP_BRIGHT_RED "(null)");
+                    pf_fprintf(stderr, GP_BRIGHT_RED "(null)");
                 break;
 
             case GP_PTR:
-                fprintf(stderr, GP_BLUE "%p", va_arg(args.list, void*));
+                pf_fprintf(stderr, GP_BLUE "%p", va_arg(args.list, void*));
                 break;
         }
-        fprintf(stderr, GP_RESET_TERMINAL "\n");
+        pf_fprintf(stderr, GP_RESET_TERMINAL "\n");
     } // end for args
     fputs("\n", stderr);
 
